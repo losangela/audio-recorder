aa06fb9df0c9f1c191c7bbddf3ee8e202f424cbf
losangela <eunsol.angela.choi@gmail.com>
2021-12-20 02:24:27 -0800

    receiving files

--- server.js
+++ server.js
@@ -1,14 +1,21 @@
 const http = require('http');
+const fs = require('fs');
 
-const hostname = '';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
+  const { url, method } = req;
+  
+  if (url === '/file' && method === 'POST') {
+    req.on('data', (data) => {
+      // save .wav file
+    })
+  }
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello World');
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+server.listen(port, () => {
+  console.log(`Server running at ${port}/`);
 });
