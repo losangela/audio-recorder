0e3d208decbacda4af0e0bae304b0a86e60e3110
losangela <eunsol.angela.choi@gmail.com>
2021-12-20 13:23:31 -0800

    adding s3 bucket access to server

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
+require('dotenv').config();
 const express = require('express');
+const aws = require('aws-sdk');
 const cors = require('cors');
 const multer = require('multer');
+const multerS3 = require('multer-s3');
+
 const app = express();
+const s3 = new aws.S3();
 const port = 4000;
 
 app.use(cors());
 app.use(express.json());
+
+aws.config.update({
+  secretAccessKey: process.env.SECRET_ACCESS_KEY,
+  accessKeyId: process.env.ACCESS_KEY_ID,
+  region: 'us-west-1'
+});
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -15,7 +26,17 @@ app.get('/recordings', (req, res) => {
   res.json('Hello World!')
 });
 
-const upload = multer({ dest: __dirname + '/public/uploads/' });
+const upload = multer({
+  storage: multerS3({
+    s3,
+    bucket: 'unearth-voice-recorder',
+    key: (req, file, cb) => {
+        console.log({s3Multer: file});
+        cb(null, file.originalname); //use Date.now() for unique file keys
+    },
+  }),
+});
+
 const type = upload.single('wav');
 
 app.post('/recording', type, (req, res) => {
